b07fd072af2998dd39aed39dcaff850b5c5ef029
Ashok Bajiya <88234185+ashok-abjima@users.noreply.github.com>
2022-05-10 22:59:35 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //    for (int i = 1; i <=10;i++)
+        //    {
+        //        Console.WriteLine(i);
+
+        //        Console.ReadKey();
+        //    }
+
+
+
+
+        //for (int i = 10; i >= 1; i--)
+        //{
+        //    Console.WriteLine(i);
+
+        //    Console.ReadKey();
+        //}
+
+
+
+        //for (int i = 1; i <= 10; i++)
+        //{   
+        //    Console.WriteLine(i*i);
+
+        //    Console.ReadKey();
+        //}
+
+        //for (int i = 1; i <= 10; i++)
+        //{
+        //    Console.WriteLine(i * i*i);
+
+        //    Console.ReadKey();
+        //}
+
+
+
+
+        //for (int i = 1; i <= 10; i++)
+        //{
+        //    Console.WriteLine(i*2);
+
+        //    Console.ReadKey();
+        //}
+
+        //for (int i = 1; i <= 10; i++)
+        //{
+        //    Console.WriteLine(i * 5);
+
+        //    Console.ReadKey();
+        //}
+
+
+        for (int i = 1; i <= 20; i++)
+        {
+
+
+            Console.WriteLine(i+2);
+            i++;
+
+            Console.ReadKey();
+        }
+
+
+        //for (int i = 1; i <= 20; i++)
+        //{
+        //    if (i % 2 == 0)
+        //    {
+        //        Console.WriteLine("Number {0} is Even", i);
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Number {0} is odd", i);
+        //    }
+        //    Console.ReadKey();
+    //}
+
+
+
+
+        
+    }
+}
